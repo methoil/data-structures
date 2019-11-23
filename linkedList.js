11e7c2a133a3604197ccdf671b65c859d3fef165
methoil <mateev.br@gmail.com>
2019-11-23 00:14:35 -0500

    linked-list: function to reverse singly linked list

--- linkedList.js
+++ linkedList.js
@@ -14,11 +14,36 @@ class linkedList {
     this.tail = newNode;
   }
 
+  appendNodes(dataArray) {
+    for (let data of dataArray) {
+      this.appendNode(data);
+    }
+  }
+
   printList(currNode) {
     if (currNode == null) {
       return;
     }
     console.log(currNode.data);
     this.printList(currNode.next);
+  }
+
+
+  reverseList() {
+    this.swapNodesRecursively(this.head, null);
+    const tempHead = this.head;
+    this.head = this.tail;
+    this.tail = tempHead;
+  }
+
+  swapNodesRecursively(node, prevNode) {
+    const nextNode = node.next;
+
+    if (nextNode !== null) {  
+      this.swapNodesRecursively(nextNode, node);
+    }
+
+    node.next = prevNode;
+    return;
   }
 }
