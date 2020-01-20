3b090febaa937fb4408309d1ab01b1be838059c7
methoil <mateev.br@gmail.com>
2020-01-19 23:04:42 -0500

    feat - undirected graph implementation

---
+++ graph.js
@@ -0,0 +1,60 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+
+  addVertex(node) {
+    if (!this.adjacentList[node]) {
+      this.adjacentList[node] = [];
+    } else {
+      console.warn(`node with value ${node} already exists`);
+    }
+  }
+  
+  addEdge(node1, node2) {
+    if (this.adjacentList[node1] && this.adjacentList[node2]) {
+      // if constructed correctly one should imply the other as this graph is undirected
+      if (!this.adjacentList[node1].includes(node2)) {
+        this.adjacentList[node1].push(node2);
+      }
+      if (!this.adjacentList[node2].includes(node1)) {
+        this.adjacentList[node2].push(node1);
+      }
+    } else {
+      console.warn("Trying to add edge for nodes that do not exist");
+    }
+  }
+
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex("0");
+myGraph.addVertex("1");
+myGraph.addVertex("2");
+myGraph.addVertex("3");
+myGraph.addVertex("4");
+myGraph.addVertex("5");
+myGraph.addVertex("6");
+myGraph.addEdge("3", "1");
+myGraph.addEdge("3", "4");
+myGraph.addEdge("4", "2");
+myGraph.addEdge("4", "5");
+myGraph.addEdge("1", "2");
+myGraph.addEdge("1", "0");
+myGraph.addEdge("0", "2");
+myGraph.addEdge("6", "5");
+
+myGraph.showConnections();
