6115b4d0ba24a3d7a7e7fd6cd1c461f896f592e0
methoil <mateev.br@gmail.com>
2020-01-21 23:08:31 -0500

    implemented bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+  for (let i = array.length; i > 1; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
+
+console.log(numbers);
+console.log("Sorting...");
+bubbleSort(numbers);
+console.log(numbers);
